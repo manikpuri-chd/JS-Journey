2f404d56a2f3f3dabe6870628fa17b7e39821393
manikpuri-chd <manikpuri.chd@gmail.com>
2026-09-07 23:29:27 +0530

    feat: set up JS environment, variables, and conditional logic
    Initialized JavaScript workspace. Completed foundational logic including data types, operators, and strict equality (===) conditionals from Apna College Lectures 1 & 2.

---
+++ first.js
@@ -0,0 +1,89 @@
+const fullName = "Manik puri";
+
+
+const PI = 3.14;
+// below is student info
+const student={
+    Name : "Ayush",
+    Class : 12,
+    Stream : "Non Medical",
+    Roll_no : 17,
+};
+/* started learning java script from
+apna college java script leture going good till now */
+
+console.log(student);
+
+/* Starting Operators
+first is Arithmetic Operators*/
+  
+let a=23
+let b=9
+
+console.log("a=", a , "& b", b); 
+console.log("a + b=", a + b); // addition
+console.log("a - b=", a - b); // subtraction
+console.log("a * b=", a * b); // multiply
+console.log("a / b=", a / b); // divide
+console.log("a % b=", a % b); // remainder
+console.log("a ** b=", a ** b); //power a^b
+
+
+// Unary Operators
+
+a--;
+console.log("a-- =" ,a);
+a++
+a++
+console.log("a++ = ", a);
+
+// Assignment Operators
+
+a = 23;
+
+console.log("a=",a);
+
+a+=4; // a= a+4
+
+
+console.log("a+=4 = ", a);
+a=23
+console.log("a %= 4 =", a%=4);
+
+// Comparison Operators
+
+a= 23
+b= 27
+
+console.log("a == b", a==b);
+
+console.log("a != b", a!=b);
+
+console.log("a > b", a > b);
+
+// Logical Operators
+
+let cond1 = a < b;
+let cond2 = a === b;
+
+console.log("cond1 || cond2  ", a<b || a === b );
+
+//Conditional - Statement
+
+age = 23;
+
+if( age > 18){
+    console.log("can vote");
+}
+
+// Ternary Operator
+
+age = 23
+
+ let result = age > 18 ? "adult" : "not adult";
+ console.log(result);
+
+
+
+
+
