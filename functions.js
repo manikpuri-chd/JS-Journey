4d05687986d6bbbc0f26a5fbfcb5b6519479c661
manikpuri-chd <manikpuri.chd@gmail.com>
2026-09-09 23:58:08 +0530

    JS journey functions

---
+++ functions.js
@@ -0,0 +1,27 @@
+function myFunction(msg){
+    console.log(msg);
+};
+
+myFunction("Learning JS!");
+
+// Function = 2 numbers sum
+
+function sum(a,b){
+    console.log(a+b)
+
+};
+
+sum( 89,92);
+
+// using return
+
+function boom(x,y){
+
+  s= x+y
+
+  return s;
+}
+
+let val = boom(3,4);
+
+console.log(val);
