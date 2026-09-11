1dcfc42f4f5cd515aa65def1e634ca9b5892d9ea
manikpuri-chd <manikpuri.chd@gmail.com>
2026-09-12 00:16:39 +0530

    Lecture Six: DESTROYED.

---
+++ DOM.js
@@ -0,0 +1,12 @@
+let para = document.querySelectorAll("p");
+console.dir(para);
+
+let child = document.querySelector("div")
+console.log(child.children);
+
+
+console.log(child.innerHTML);
+
+console.log(child.innerText);
+
+console.log(child.innerText="abcdeeerertsfks");
